3c6360e414c459572d80673adf73ab2c3cb12c09
kgkavishka0-dev <kgkavishka0@gmail.com>
2026-09-26 08:18:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,19 @@ require('events').EventEmitter.defaultMaxListeners = 500;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/code', code);
+// pair.js එක function එකක් නම් app.get භාවිතා කරන්න:
+app.get('/code', code);
 
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html')
+app.get('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html');
 });
 
-app.use('/settings', async (req, res, next) => {
-    res.sendFile(__path + '/settings.html')
+app.get('/settings', async (req, res, next) => {
+    res.sendFile(__path + '/settings.html');
 });
 
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
+app.get('/', async (req, res, next) => {
+    res.sendFile(__path + '/main.html');
 });
 
 app.listen(PORT, () => {
